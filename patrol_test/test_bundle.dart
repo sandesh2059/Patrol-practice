a15848ef48cdab29c516a74569eb7b67d812397d
Sandesh <sandeshchy2059@gmail.com>
2026-08-20 16:32:09 +0545

    readme file added

--- patrol_test/test_bundle.dart
+++ patrol_test/test_bundle.dart
@@ -13,7 +13,7 @@ import 'package:patrol/src/platform/mobile/patrol_runtime_ports.dart';
 import 'package:test_api/src/backend/invoker.dart';
 
 // START: GENERATED TEST IMPORTS
-import 'Create_account_test.dart' as Create_account_test;
+import 'login_test.dart' as login_test;
 // END: GENERATED TEST IMPORTS
 
 Future<void> main() async {
@@ -105,7 +105,7 @@ Future<void> main() async {
   });
 
 // START: GENERATED TEST GROUPS
-  group('Create_account_test', Create_account_test.main);
+  group('login_test', login_test.main);
 // END: GENERATED TEST GROUPS
 
   if (constants.testDiscoveryEnabled) {
